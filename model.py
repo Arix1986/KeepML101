c4e5e6f78fe57402e92a002034f2666941110af8
Arix86 <arix86.solutions@gmail.com>
2024-12-13 17:22:31 -0300

    Mejora en el modelo LightGBM .7784

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
 import numpy as np
-from sklearn.ensemble import ExtraTreesRegressor, GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor,ExtraTreesRegressor
 from lightgbm import LGBMRegressor
-from skopt import BayesSearchCV
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import r2_score
-from utils import model_exports  
-from skopt.space import Categorical
 from xgboost import XGBRegressor
-
+from sklearn.ensemble import BaggingRegressor
+from sklearn.model_selection import GridSearchCV
+import joblib
 class Models:
     
     def __init__(self):
@@ -14,34 +15,62 @@ class Models:
         self.X_test = None
         self.y_test = None
         self.reg = {
-             'LightGBM':LGBMRegressor(force_row_wise=True),
-              'XGBoost': XGBRegressor(),
-            'GradientBoosting': GradientBoostingRegressor(),
-           
+               'ExtraTrees':ExtraTreesRegressor(random_state=42),
+               'LightGBM':LGBMRegressor(force_row_wise=True,random_state=42),
+              'GradientBoosting': GradientBoostingRegressor(random_state=42),
+              'XGBoost': XGBRegressor(random_state=42),
+              'BagginRegressor_KN':BaggingRegressor(estimator=KNeighborsRegressor(),random_state=42),
+              'BagginRegressor_DT':BaggingRegressor(estimator=DecisionTreeRegressor(),random_state=42),
             
-        }
+            
+           
+         }
         
         self.params = {
             
             'LightGBM': {
-                'n_estimators': [100, 200, 500],
-                'learning_rate': [0.01, 0.02, 0.03,0.04 ,0.05, 0.1],
-               'num_leaves': [20, 31, 50, 100],
-                'max_depth': [-1, 5, 9, 12],
-                
+                'boosting_type': ['dart'],
+                'learning_rate': [0.01, 0.02, 0.03,0.032,0.04 ,0.05, 0.1],
+                'n_estimators': [500],
+                'num_leaves': [21, 31,41, 53],
+                'max_depth': [ 7,9,10,11,12],
+                'min_child_samples':[20],
+                'min_child_weight':[0.001],
+                'reg_alpha': [0.0], 
+                'reg_lambda': [0.0],
+                             
             },
             'XGBoost': {
                 'n_estimators': [100, 200, 500],
                 'learning_rate': (0.01,0.02, 0.03,0.05,0.1,0.2,0.3,0.4 ),
                 'max_depth': [5,9, 10, 12],
+            
             },
             'GradientBoosting': {
-                'n_estimators': [100, 200, 500],
-                 'learning_rate': (0.01,0.02, 0.03,0.05,0.1,0.2,0.3,0.4 ),
-                'max_depth': [3,5, 9, 12],
-                'min_samples_split': [2, 5,8],
-                'min_samples_leaf': [1, 2, 4]
+                'n_estimators': [100, 200, 300,500],
+                'learning_rate': (0.01,0.02, 0.03,0.05,0.1,0.2,0.3,0.4 ),
+                'max_depth': [3, 5,9, 12],
+                
             },
+            'BagginRegressor_KN': {
+                'n_estimators': [50, 100, 200],          
+                'max_samples': [ 1.0],                
+                'bootstrap': [True, False],                   
+                'bootstrap_features': [True, False],           
+             },
+            'BagginRegressor_DT': {
+                'n_estimators': [50, 100, 200],          
+                'max_samples': [ 1.0],                
+                'estimator__max_depth':[5,9,10] ,             
+                'bootstrap': [True, False],                   
+                'bootstrap_features': [True, False],           
+             },
+             
+            'ExtraTrees': {
+                 'n_estimators': [100, 200, 300,500],
+                'ccp_alpha': (0.01,0.02, 0.03,0.05,0.1,0.2,0.3,0.4 ),
+                'max_depth': [3, 5,9, 12],
+             },  
           
         }
 
@@ -58,7 +87,7 @@ class Models:
         
         for name, reg in self.reg.items():
             print(f"Training model: {name}")
-            bayes_search = BayesSearchCV(reg, self.params[name], cv=5, n_iter=50, random_state=42, verbose=0)
+            bayes_search = GridSearchCV(reg, self.params[name], cv=5,  verbose=2)
             bayes_search.fit(X, y.values.ravel())
             
             score = self.evaluate_model(name, bayes_search, self.X_test, self.y_test)
@@ -69,7 +98,16 @@ class Models:
                 print(f"New best model: {name} with score {best_score:.4f}")
         
         self.best_model = best_model
-        model_exports(best_model, best_score)
+        self.model_exports(best_model, best_score)
+        self.model_grilla(bayes_search)
+        
         
     def get_best_model(self):
         return self.best_model
+    
+    def model_exports(self, clf,score):
+        score=str(score)
+        joblib.dump(clf,f'./models/best_model_{score[:5]}.pkl')
+    
+    def model_grilla(self, clf):
+        joblib.dump(clf,f'./models/best_model_grilla.pkl')   
